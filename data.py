eec69ea8b59140a6ae4aaa5ef21e6032f651d05f
Huyen Phan <128070162+HuyenPhan-ComputerScience@users.noreply.github.com>
2023-11-17 19:14:08 -0800

    changes in real data time route in backend

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import yfinance as yf
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -7,6 +9,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+#getring history stock of the commpany
 @app.route('/historical', methods=['GET', 'POST'])
 def historical_data():
     if request.method == 'POST':
@@ -25,20 +28,39 @@ def historical_data():
 
     return render_template('historical_form.html')
 
-@app.route('/realtime', methods=['GET', 'POST'])
-def real_time_data():
+#real time stock price
+def web_content_div(web_content, class_path):
+    web_content_div = web_content.find('div', class_=class_path)
+    try:
+        fin_stream = web_content_div.find_all('fin-streamer')  
+        texts = [fin.get_text() for fin in fin_stream]
+    except AttributeError:
+        texts = []
+    return texts
+
+def real_time_price(stock_code):
+    url = 'https://finance.yahoo.com/quote/' + stock_code + '?p=' + stock_code + '&.tsrc=fin-srch'
+    try:
+        r = requests.get(url)
+        r.raise_for_status()  # Raise an HTTPError for bad requests
+        web_content = BeautifulSoup(r.text, 'html.parser')
+        texts = web_content_div(web_content, 'My(6px) Pos(r) smartphone_Mt(6px) W(100%)')
+        if texts:
+            price, change, percent = texts[0], texts[1], texts[2]
+        else:
+            price, change, percent = None, None, None
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        price, change, percent = None, None, None
+    return price, change, percent
+
+#Route to real time stock price
+@app.route('/realtime',methods=['GET', 'POST'])
+def index():
     if request.method == 'POST':
-        stock_symbol = request.form['stock_symbol']
-
-        try:
-            # Fetch real-time data using yfinance
-            stock_info = yf.Ticker(stock_symbol)
-            current_price = stock_info.info['last_price']
-        except:
-            current_price = "N/A"  # Handle the case where real-time data is not available
-
-        return render_template('realtime_data.html', stock_symbol=stock_symbol, current_price=current_price)
-
+        stock_code = request.form['stock_code']
+        price, change, percent = real_time_price(stock_code)
+        return render_template('realtime.html', price=price, change=change, percent=percent)
     return render_template('realtime_form.html')
 
 if __name__ == '__main__':
