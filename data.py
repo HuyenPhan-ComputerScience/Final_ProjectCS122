b10839cd211b79f33c27b26e02aeb0ca23d362d6
Huyen Phan <128070162+HuyenPhan-ComputerScience@users.noreply.github.com>
2023-11-28 00:05:56 -0800

    data modify on history and create home route

--- data.py
+++ data.py
@@ -1,34 +1,74 @@
 from flask import Flask, render_template, request
 import yfinance as yf
+import pandas as pd
+import datetime
 import requests
-from bs4 import BeautifulSoup
 
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+import matplotlib
+matplotlib.use('agg')
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    
+    df=pd.read_csv('data/sp500.csv')
 
-#getring history stock of the commpany
-@app.route('/historical', methods=['GET', 'POST'])
-def historical_data():
-    if request.method == 'POST':
-        stock_symbol = request.form['stock_symbol']
+    return render_template('home.html',column_names=df.columns.values, row_data=list(df.values.tolist()),
+                           link_column="Symbol", zip=zip)
+ 
+
+@app.route('/ticker/<ticker>', methods=['GET', 'POST'])
+def ticker(ticker):
+    if request.method == 'GET':
+        return render_template('ticker_form.html', stock_symbol=ticker)
+    else:
+        stock_symbol =  ticker
         start_date = request.form['start_date']
         end_date = request.form['end_date']
-
+        print(stock_symbol, start_date, end_date)
         # Fetch historical data using yfinance
         df = yf.download(stock_symbol, start=start_date, end=end_date)
 
-        # Process data as needed
+        # # Check if the DataFrame is empty
+        if df.empty:
+            return render_template('ticker.html', stock_symbol=stock_symbol, data=None, plot_url=None)
+        
+        print(df)
+            # Process data as needed
         # For example, you can convert the DataFrame to a list of dictionaries
         data = df.reset_index().to_dict(orient='records')
 
-        return render_template('historical_data.html', stock_symbol=stock_symbol, data=data)
+        # Plotting the data
+        plt.figure(figsize=(10, 6))
+        plt.plot(df.index, df["Close"], label='Closing Price')
+        plt.xlabel('Date')
+        plt.ylabel('Closing Price')
+        plt.title(f'Historical Stock Data for {ticker}')
+        plt.legend()
 
-    return render_template('historical_form.html')
+        # Save the plot to a BytesIO object
+        img = BytesIO()
+        plt.savefig(img, format='png')
+        img.seek(0)
 
-#real time stock price
+        # Encode the image as base64 and convert it to a string
+        plot_url = base64.b64encode(img.getvalue()).decode()
+
+        return render_template('ticker.html', stock_symbol=stock_symbol, data=data, plot_url=plot_url)
+
+#getting information about the company
+
+
+
+
+
+
+#real time data
+
 def web_content_div(web_content, class_path):
     web_content_div = web_content.find('div', class_=class_path)
     try:
@@ -54,7 +94,6 @@ def real_time_price(stock_code):
         price, change, percent = None, None, None
     return price, change, percent
 
-#Route to real time stock price
 @app.route('/realtime',methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -62,6 +101,10 @@ def index():
         price, change, percent = real_time_price(stock_code)
         return render_template('realtime.html', price=price, change=change, percent=percent)
     return render_template('realtime_form.html')
+
+
+
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
